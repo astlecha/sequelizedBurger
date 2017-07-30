0032c9a2cc0b872790af4ba4c4f18f79071f1f2d
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-30 14:48:41 -0700

    path to main handlebars file

---
+++ routes/html-routes.js
@@ -0,0 +1,8 @@
+//Built-in node dependency for file paths 
+var path = require("path");
+
+module.exports(function(app){
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, "../views/layouts/main.handlebars"));
+	});
+})
