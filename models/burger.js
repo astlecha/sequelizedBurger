bd751a447d9bf0c08e40625e807b7f1b4d08120d
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-30 14:29:32 -0700

    creates burger model

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,21 @@
-//Import ORM
-var orm = require('../config/orm.js');
-//Create function to be used with database
-var burger = {
-	selectAll: function(cb) {
-	    orm.selectAll('burgers', function(res){
-	    	cb(res);
-	    });
-	},
-	//Account for arrays - colName and vals
-	insertOne: function(colNames, vals, cb){
-		orm.insertOne('burgers', colNames, vals, function(res){
-			cb(res);
-		});
-	},
-	updateOne: function(objColVals, condition, cb){
-		orm.updateOne('burgers', objColVals, condition, function(res){
-			cb(res);
-		});
-	}
-};
-
-//Export model to be used in burgers_controller.js
-module.exports = burger;
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    createdAt: {
+      type: DataTypes.TIMESTAMP,
+      defaultValue: CURRENT_TIMESTAMP
+    }
+  });
+  return Burger;
+}
