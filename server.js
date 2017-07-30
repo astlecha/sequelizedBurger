fb7c6142c5b243076c1d9d31ec1313e3a0860e6e
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-30 14:11:15 -0700

    eliminates handlebars and method-override

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
+// Require depedencies
 var express = require('express');
-var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
-var port = process.env.port || 3000;
-
+//Establish port and express app
+var port = process.env.port || 8080;
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+// Requiring our models for syncing
+var db = require("./models");
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Set Handlebars.
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Static directory
+app.use(express.static("public"));
+
 
 //Import routes and give the server access to them.
 var routes = require('./controllers/burgers_controller.js');
